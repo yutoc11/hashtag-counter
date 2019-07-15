48dd74cbcef069d5ed14d4063599d9112b69e173
utoc11 <yutoc11@gmail.com>
2019-07-15 22:52:55 +0900

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  v-app
+    v-toolbar.white--text(color="pink darken-2")
+      app
+
+      v-toolbar-side-icon.white--text
+      v-toolbar-title #カウンター
+
+    v-content
+      v-container
+      nuxt/
+
+    v-footer.white--text(color="pink darken-2")
+      app
+
+      span #カウンター
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.section{
+  margin: 0 auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+  section.section
+    .columns.is-mobile ああ
+    nuxt-link(to="/") Home page
+      br
+    nuxt-link(to="inspire") Inspire
+    v-flex.xs6.md6
+      v-card
+        v-card-title.font-weight-bold 新規会員登録フォーム(3分で完了)
+</template>
+
+<script>
+import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {
+    Card
+  }
+}
+</script>
+<style lang="scss">
+.section{
+  margin: 0 auto;
+}
+</style>
