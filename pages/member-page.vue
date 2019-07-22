04e1cffa370c3c0d5914c60c487f5f7e49216e10
yutoc11 <kikuchi-yuto@sagojo.link>
2019-07-22 23:02:45 +0900

    firebaseの導入と、仮のページでログイン、ログイン判定、ログアウトを実装テストまで完了。

---
+++ pages/member-page.vue
@@ -0,0 +1,15 @@
+<template>
+  <members-only>
+    <h1>ログインしていないと見れないページ</h1>
+    <a href="/login/login_test">アカウントページへ</a>
+  </members-only>
+</template>
+
+<script>
+import MembersOnly from '~/components/members-only.vue'
+
+export default {
+  components: {
+    MembersOnly
+  }
+}
