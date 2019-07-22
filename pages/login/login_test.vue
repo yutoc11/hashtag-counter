c8619da1e009ae67f514c52f41badf3c217279e2
yutoc11 <kikuchi-yuto@sagojo.link>
2019-07-22 23:02:45 +0900

    firebaseの導入と、仮のページでログイン、ログイン判定、ログアウトを実装テストまで完了。

---
+++ pages/login/login_test.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <!-- ログイン中に表示される画面 -->
+    <div v-if="isAuthenticated">
+      {{ user.email }}でログイン中です<br>
+      <button v-on:click="logout">ログアウト</button><br>
+      <a href="/member-page">メンバーページへ</a>
+    </div>
+    <!-- ログインしていない時に表示される画面 -->
+    <div v-else>
+      メール<br>
+      <input type="text" v-model="email"><br>
+      パスワード<br>
+      <input type="password" v-model="password"><br>
+      <button v-on:click="login">ログイン</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from '~/plugins/firebase'
+import { mapActions, mapState, mapGetters } from 'vuex'
+export default {
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  computed: {
+    ...mapState(['user']),
+    ...mapGetters(['isAuthenticated'])
+  },
+  mounted() {
+    firebase.auth().onAuthStateChanged((user) => {
+      this.setUser(user)
+    })
+  },
+  methods : {
+    ...mapActions(['setUser']),
+    login() {
+      firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+      .then(user => {
+        // ログインしたら飛ぶページを指定
+        // this.$router.push("/member-page")
+      }).catch((error) => {
+        alert(error)
+      });
+    },
+    logout() {
+      firebase.auth().signOut()
+      .then(() => {
+        this.setUser(null)
+      }).catch((error) => {
+        alert(error)
+      })
+    }
+  }
+}
+</script>
