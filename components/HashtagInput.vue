486969f5ebc02f9420e90eda49b8cd9c06668e6f
yutoc11 <kikuchi-yuto@sagojo.link>
2019-07-26 21:06:03 +0900

    コンポーネントでの#カウント表示がうまくいかないのでindex.vueに直接記載する対応。

--- components/HashtagInput.vue
+++ components/HashtagInput.vue
@@ -1,3 +1,4 @@
+<!-- うまくいかないから一旦index.vueに直接入れてる -->
 <template lang="pug">
   v-layout(justify-center)
     v-flex(xs12 md8)
@@ -8,10 +9,11 @@
           )
         v-textarea(
           placeholder="Instagramに載せたいハッシュタグの羅列をご入力ください。"
+          v-model="message"
           solo
           )
         v-container
-          p.headline.text-xs-center.pink--text.font-weight-bold #の数：30
+          p.headline.text-xs-center.pink--text.font-weight-bold #の数：{{ now_hashtag_count }}
 
         v-layout(align-center justify-space-between row fill-height)
           v-btn(round) クリア
@@ -24,14 +26,16 @@
 
 <script>
 export default {
-  props: {
-    title: {
-      type: String,
-      required: true
-    },
-    icon: {
-      type: String,
-      required: true
+
+  data () {
+    return {
+      now_hashtag_count:'',
+    };
+  },
+
+  computed: {
+    now_hashtag_count() {
+      return this.message.length;
     }
   }
 }
