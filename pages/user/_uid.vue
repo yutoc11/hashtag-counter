fe9ebeef2502a92daa22de99af786e5b6cbf7a71
yutoc11 <kikuchi-yuto@sagojo.link>
2019-08-11 08:56:04 +0900

    マイページを少し整理

--- pages/user/_uid.vue
+++ pages/user/_uid.vue
@@ -6,9 +6,9 @@
             v-card-title(primary-title)
               h4 プロフィールを変更する
             v-form
-              v-text-field(prepend-icon="person" name="email" label="メールアドレス" type="text" :value="user.email")
+              v-text-field(prepend-icon="person" name="email" label="メールアドレス" type="text" :value="email")
               v-text-field(prepend-icon="lock" name="password" label="現在のパスワード" type="password" :value="password")
-              v-text-field(prepend-icon="lock" name="password_confirm" label="パスワード確認用" type="password")
+              v-text-field(prepend-icon="lock" name="password_confirm" label="パスワード確認用" type="password" :value="password_confirm")
               v-card-actions
                 v-btn(primary block round @click="changeProfile") 変更する（まだできない）
     v-layout.mb-5.text-xs-center(justify-end)
@@ -32,7 +32,6 @@ export default {
       email: '',
       password: '',
       password_confirm: '',
-
     };
   },
 
@@ -43,7 +42,6 @@ export default {
   },
 
   components: {
-
   },
 
   computed: {
@@ -57,10 +55,6 @@ export default {
         if (user) {
           this.setUser(user)
           this.user = user
-          console.log(user)
-        }else{
-          this.setUser(null)
-          this.$router.push("/")
         }
     })
   },
@@ -71,22 +65,28 @@ export default {
   methods: {
     ...mapActions(['setUser']),
 
+    //ログアウト
     logOut(){
       firebase.auth().signOut()
       this.setUser(null)
       this.$router.push('/?flash=logout')
     },
 
+    //退会（ユーザーデータの削除）
     deleteUser(){
       var user = firebase.auth().currentUser;
-      user.delete().then(function() {
+      user.delete().then(
+        function() {
         // User deleted.
-        }).catch(function(error) {
-          // An error happened.
-          });
+        }
+      ).catch(
+        function(error) {
+        // An error happened.
+        }
+      );
       this.$router.push('/?flash=delete')
-      }
-  }
+      },
+  },
 }
 </script>
 
